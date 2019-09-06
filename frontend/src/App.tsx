2b475d92f9ea0bf2533415e7656a1ff99a02e92e
stobita <tobita.pc@gmail.com>
2019-09-06 22:55:43 +0900

    frontend basic layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext } from "react";
+import "./App.css";
+import { Home } from "./components/Home";
+import styled, { ThemeProvider } from "styled-components";
+import Provider, { MainContext } from "./Provider";
+import theme from "./theme";
 
 const App: React.FC = () => {
+  const { themeName } = useContext(MainContext);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Provider>
+        <ThemeProvider theme={theme[themeName]}>
+          <Home />
+        </ThemeProvider>
+      </Provider>
     </div>
   );
-}
+};
 
 export default App;
---
+++ frontend/src/colors.ts
@@ -0,0 +1,9 @@
+export default {
+  white: "#fff",
+  mainWhite: "#fcfcfc",
+  black: "#000",
+  mainBlack: "#111",
+  mainGray: "#666",
+  thinGray: "#CCC",
+  borderGray: "#ddd"
+};
